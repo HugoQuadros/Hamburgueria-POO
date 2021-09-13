c455f88a8cd6593980550a4ca664f9848f722020
Hugo Quadros <83436359+HugoQuadros@users.noreply.github.com>
2021-09-13 14:28:55 -0300

    criando classe produto

---
+++ src/Produto.java
@@ -0,0 +1,30 @@
+public class Produto {
+
+    private String nome;
+    private String tipo;
+    private String tamanho;
+    private double valor;
+
+    public String getNome() {return nome;}
+    public void setNome(String nome) {this.nome = nome;}
+    public String getTipo() {return tipo;}
+    public void setTipo(String tipo) {this.tipo = tipo;}
+    public String getTamanho() {return tamanho;}
+    public void setTamanho(String tamanho) {this.tamanho = tamanho;}
+    public double getValor() {return valor;}
+    public void setValor(double valor){this.valor = valor;}
+
+    public Produto(){
+        this.setNome("");
+        this.setTamanho("");
+        this.setTipo("");
+        this.setValor(0.0);
+    }
+
+    public Produto (String nome, String tipo, String tamanho){
+        this.setNome(nome);
+        this.setTipo(tipo);
+        this.setTamanho(tamanho);
+        this.setValor(10.0);
+    }
+}
